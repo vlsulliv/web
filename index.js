6c64462e1448f3a9d259a31364b615ef2671085d
vlsulliv <vsulliv@pm.me>
2023-07-27 20:36:20 -0500

    add html structure

---
+++ index.js
@@ -0,0 +1,17 @@
+exports.printMsg = function() {
+    console.log("This is a message from the demo package");
+  }
+
+// module.exports = {
+//     globals: {
+//         MyGlobal: true
+//     },
+//     rules: {
+//         semi: [2, "always"]
+//     }
+// };
+
+// const helloYou = (name) => {
+// 	name = "you" || name;
+// 	console.log("hello" + name + "!");
+// };
